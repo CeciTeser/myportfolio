88fa6eb1e8b50598c11c0761562cf1c3138d3a37
Austyn <austyn@Air-de-Austyn.fibertel.com.ar>
2022-01-30 21:05:17 +0100

    portfolio´s structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Profile } from './components/profile';
+import {BrowserRouter as Router } from 'react-router-dom'
+import { Matcheadas } from './components/matcheadas';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Router>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-2">
+            <Profile/>
+          </div>
+          <div className="col-8 ms-5">
+            <Matcheadas/>
+          </div>
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
 export default App;
+
---
+++ src/components/profile/index.tsx
@@ -0,0 +1,59 @@
+import ceci from '../../assets/img/profile/ceci.jpg';
+
+import './style.scss';
+
+const Profile =()=>{
+    return (
+        <div className="profile">
+            
+            <img src={ceci} alt="profile" />
+         
+            <h4 className='mt-3'>CECI TESER</h4>
+            <h5>FRONTEND DEVELOPER</h5>
+           
+            <p className='pt-3'>Currently finishing the Frontend developer career at ADA ITW and looking for the first experiences as Trainee or Junior developer.</p>
+        
+
+            <div className='mt-5'>
+                <h3>PROGRAMMING SKILLS</h3>
+                <ul className='pt-2'>
+                    <li>HTML5</li> 
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Git</li>
+                    <li>GitHub</li>
+                    <li>TypeScript</li>
+                    <li>Responsive design</li>
+                    <li>Api REST</li>
+                    <li>Firebase</li>
+                    <li>Bootstrap</li>
+                    <li>Sass</li>
+                    <li>React js</li>
+                    <li>Redux</li>
+                </ul>
+            </div>
+
+            <div className='mt-5'>
+                <h3>LANGUAGE</h3>
+                <ul className='pt-2'>
+                    <li>English bilingual</li>
+                    <li>Spanish native</li>
+                    <li>Francés: 1er degreé</li>
+                </ul>
+            </div>
+
+            <div className='mt-5'>
+                <h3>CONTACT</h3>
+                <ul className='pt-2'>
+                    <li> <a href="mailto:mcteser@gmail.com<" target="_blank">mcteser@gmail.com</a></li>
+
+                    <li> <a href="https://www.linkedin.com/in/ceciliateisaire/" target="_blank" >linkedin:/ceciliateisaire</a></li>
+
+                    <li> <a href="https://github.com/CeciTeser" target="_blank">github.com/CeciTeser</a></li>
+                </ul>
+           </div>
+        </div>
+    )
+}
+
+export { Profile }
